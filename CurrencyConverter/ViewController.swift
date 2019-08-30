61c89e84f7f6ac4e9ed1775858a183d5bdd3871c
Вячеслав Живетьев <slavyan6363@gmail.com>
2019-08-30 19:58:51 +0300

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Вячеслав Живетьев on 30/08/2019.
+//  Copyright © 2019 Вячеслав Живетьев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
