347cf9e06286d7575da8e95f69c77ae8f6719303
Вячеслав Живетьев <slavyan6363@gmail.com>
2019-08-30 20:06:40 +0300

    removed storyboard

--- CurrencyConverter/AppDelegate.swift
+++ CurrencyConverter/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+		
+		let window = UIWindow()
+		let navigationController = UINavigationController()
+		
+		self.window = window
+		window.rootViewController = navigationController
+		window.makeKeyAndVisible()
+		
 		return true
 	}
 
